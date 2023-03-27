327b759f4ae06098d61ad84aa4796f09f33f1901
Kgotso Makhalimele <kgotsom@protonmail.com>
2023-03-27 22:28:43 +0200

    completed challenges

---
+++ challenge-1.js
@@ -0,0 +1,25 @@
+
+/* This is the firstname of the user /*
+
+const user = 'John'
+
+
+/* This is the lastname of the user /* const surname = 'Smith'
+
+
+console.log(user; surname;)
+
+
+// Start of solution
+
+
+/* This is the firstname of the user */
+
+const user = 'John'
+
+
+/* This is the lastname of the user */ 
+const surname = 'Smith'
+
+
+console.log(user, surname)
